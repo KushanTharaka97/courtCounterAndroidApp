5e1b7c35d05f0b3841a9ae8afc0ea76c8ace669b
tharaka <tharakakushan@gmail.com>
2020-06-09 14:07:32 +0530

    Bug Fixed: Some initializations in the ManifestFiles was not clearly defined.I have corrected those things.

--- app/src/main/java/com/example/android/court_counterappkt2020/MainActivity.java
+++ app/src/main/java/com/example/android/court_counterappkt2020/MainActivity.java
@@ -21,8 +21,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.example.android.courtcounter.R;
+
 
 /**
  * This activity keeps track of the basketball score for 2 teams.
